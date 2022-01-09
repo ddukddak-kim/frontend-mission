ae5ebf422a530608abb7a443611cf721d906b7fd
kmkim <kkgg7527@gmail.com>
2022-01-09 17:37:49 +0900

    test: add notice modal visible test

--- mission/tests/unit/main.spec.js
+++ mission/tests/unit/main.spec.js
@@ -1,14 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
 import Main from '@/components/Main.vue';
+import Notice from '@/components/modals/Notice.vue';
 
-const wrapper = shallowMount(Main);
+
 describe('Main.vue', () => {
-  it('렌더링 완료.', () => {
-      // const wrapper = shallowMount(Main);
-      // console.log("--->", wrapper.html());
-  });
 
-  test('submit test', async () => {
+  test('제출 버튼 두 번 클릭시 올바른 결과값.', async () => {
+    const wrapper = shallowMount(Main);
     const $submitBtn = wrapper.find('button.submit');
 
     const GIVEN_INPUT = '1234';
@@ -22,4 +20,15 @@ describe('Main.vue', () => {
     expect(wrapper.text()).toContain(EXPEXTED_RESULT);
   });
 
+
+  test('알림버튼 클릭 시, 모달 정상 출력 여부.', async () => {
+    const wrapper = shallowMount(Main);
+    const NoticeModal = shallowMount(Notice);
+    
+    const $noticeBtn = wrapper.find('button.notice');
+   
+    await $noticeBtn.trigger('click');
+
+    expect(NoticeModal.isVisible()).toBe(true);
+  });
 });
