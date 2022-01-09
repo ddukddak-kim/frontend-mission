f9014dea7ef18083f74a2575500de5b951b94a98
kmkim <kkgg7527@gmail.com>
2022-01-09 12:30:00 +0900

    feat: edit html template, add confirm action

--- mission/src/components/HelloWorld.vue
+++ mission/src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="hello">
+  <div class='hello'>
     <h1>{{ msg }}</h1>
+
+    <div>
+      <input type='text' v-model='inputText'/>
+      <button v-on:click='confirm'>제출</button>
+      <button v-on:click='alarm'>알림</button>
+    </div>
+
+    <div>
+      <h2> {{ inputText }} </h2>
+    </div>
+
   </div>
 </template>
 
@@ -10,10 +21,22 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  methods: {
+    confirm() {
+      this.inputText = this.inputText.substr(1) + this.inputText.substr(0, 1);
+    },
+    alarm() {
+    },
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
