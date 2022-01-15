fe6ed84ac345d4ef76c8f0dc3fa2ba93992b7ec7
kmkim <kkgg7527@gmail.com>
2022-01-16 04:08:09 +0900

    feat: add data-attribute to Title-component

--- mission/src/components/Title.vue
+++ mission/src/components/Title.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <div class="title-image">img</div>
+    <div class="title-image">{{ titleImage }}</div>
 
     <div class="store">
-      <div>img</div>
+      <div>{{ store.image }}</div>
       <div>
         <div>대한양복</div>
         <div>#tag1 #tag1</div>
       </div>
       <div>즐겨찾기</div>
     </div>
-
-    <div class="price">
-      <div class="discount-percent">34%</div>
-      <div>198,000 원</div>
-      <div>200,000 원</div>
+    <div class="product">
+      <div class="name">
+        <a>{{ product.name }}</a>
+      </div>
+      <div class="price">
+        <div class="discount-percent">{{ price.discountPercent }}%</div>
+        <div class="discount-price">{{ price.discountPrice }} 원</div>
+        <div class="origin-price">
+          <del>{{ price.originPrice }} 원</del>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +28,24 @@
 <script>
 export default {
   name: 'Title',
+  data() {
+    return {
+      titleImage: 'img1',
+      store: {
+        image: 'img2',
+        name: '',
+        hashs: [],
+      },
+      product: {
+        name: '핏이 좋은 수트',
+      },
+      price: {
+        discountPercent: 34,
+        discountPrice: '198000',
+        originPrice: '200000',
+      },
+    };
+  },
 };
 </script>
 
@@ -47,15 +71,29 @@ export default {
 .store div:nth-child(3) {
   min-width: 15%;
 }
-.price {
+.product {
   margin: 10px;
+}
+.product .name {
   display: flex;
   justify-content: flex-start;
+  font-size: 1.5em;
 }
-.price div {
-  margin-right: 10px;
+.product .price {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 }
-.price .discount-percent {
+.product .price div {
+  font-size: 1.3em;
+  margin-right: 15px;
+}
+.product .price .discount-percent {
   margin-top: 3px;
+  color: #f54949;
+}
+.product .price .origin-price {
+  font-size: 0.8em;
+  color: #b3b3b3;
 }
 </style>
