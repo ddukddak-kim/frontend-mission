9fe765e332b7f8bc61571d7a21c621c69c4753a5
kmkim <kkgg7527@gmail.com>
2022-01-16 17:37:11 +0900

    edit: change v-for element to v-html at product detail

--- mission/src/components/Detail.vue
+++ mission/src/components/Detail.vue
@@ -4,7 +4,7 @@
     <div class="sub-title info">{{subTitle}}</div>
     <div v-for="(item, index) of details" :key="index">
       <img class="detail-image" :src="item.image" alt= />
-      <div class="detail-info info">{{ item.description }}</div>
+      <div class="detail-info info" v-html="item.description"></div>
     </div>
   </div>
 </template>
@@ -22,7 +22,7 @@ export default {
         },
         {
           image: require('../assets/img/detail/detail_02.jpg'),
-          description: '부드러운 터치감과 고급스러운 컬러감을 제공합니다.',
+          description: '부드러운 터치감과<br>고급스러운 컬러감을 제공합니다.',
         },
       ],
     };
