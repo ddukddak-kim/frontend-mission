6f2b13800f5891aa4a1a16c98e6574368138b850
kmkim <kkgg7527@gmail.com>
2022-01-16 18:13:27 +0900

    feat: add purchase button position fixed css

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -3,6 +3,7 @@
     <Title />
     <Detail />
     <Review />
+    <Purchase class="purchase" price="299,000" />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Title from '@/components/Title.vue';
 import Detail from '@/components/Detail.vue';
 import Review from '@/components/Review.vue';
+import Purchase from '@/components/Purchase.vue';
 
 export default {
   name: 'ItemInfoPage',
@@ -17,6 +19,7 @@ export default {
     Title,
     Detail,
     Review,
+    Purchase,
   },
   data() {
     return {};
@@ -28,6 +31,22 @@ export default {
 
 <style scoped>
 #item-info-page {
-  margin: -8px;
+  margin: -8px 0 50px 0;
+}
+
+.purchase {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  min-height: 50px;
+  background: #fff;
+  border-top: 1px solid #ddd;
+  /* position: fixed;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  min-height: 50px;
+  background: red; */
 }
 </style>
