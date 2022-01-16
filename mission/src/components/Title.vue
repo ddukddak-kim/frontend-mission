1d9daa4090f2cbfddf5bfd75dfc19dbbea00443d
kmkim <kkgg7527@gmail.com>
2022-01-16 15:02:33 +0900

    feat: edit title-component css

--- mission/src/components/Title.vue
+++ mission/src/components/Title.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-    <div class="title-image">{{ titleImage }}</div>
+    <div class="title-image">
+      <img :src="product.image" alt="product-image" />
+    </div>
 
     <div class="store">
-      <div>{{ store.image }}</div>
+      <div class="image" :style="{'background-image': `url('${store.image}')`}"></div>
       <div>
-        <div>대한양복</div>
-        <div>#tag1 #tag1</div>
+        <div class="name">{{ store.name }}</div>
+        <div class="hash">
+          <a v-for="(item,index) of store.hash" :key="index"># {{item.name}}</a>
+        </div>
       </div>
       <div>즐겨찾기</div>
     </div>
+
     <div class="product">
       <div class="name">
         <a>{{ product.name }}</a>
       </div>
+
       <div class="price">
         <div class="discount-percent">{{ price.discountPercent }}%</div>
         <div class="discount-price">{{ price.discountPrice }} 원</div>
@@ -30,13 +36,22 @@ export default {
   name: 'Title',
   data() {
     return {
-      titleImage: 'img1',
       store: {
-        image: 'img2',
-        name: '',
-        hashs: [],
+        image: require('../assets/img/title/store_01.jpg'),
+        name: '대한양복',
+        hash: [
+          {
+            name: 'tag1',
+            code: '01',
+          },
+          {
+            name: 'tag2',
+            code: '02',
+          },
+        ],
       },
       product: {
+        image: require('../assets/img/title/title_01.jpg'),
         name: '핏이 좋은 수트',
       },
       price: {
@@ -46,31 +61,55 @@ export default {
       },
     };
   },
+  methods: {},
+  computed: {},
 };
 </script>
 
 <style>
 .title-image {
-  border: 1px solid rebeccapurple;
-  min-height: 300px;
 }
+.title-image img {
+  width: 100%;
+}
+
 .store {
   min-height: 60px;
-  margin: 5px;
+  padding: 10px 15px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   border-bottom: 1px solid gainsboro;
 }
+.store div {
+  margin-right: 10px;
+}
 .store div:nth-child(1) {
   min-width: 15%;
 }
 .store div:nth-child(2) {
-  min-width: 70%;
+  min-width: 50%;
 }
 .store div:nth-child(3) {
   min-width: 15%;
 }
+.store div:last-child {
+  margin-right: 0px;
+}
+.store .image {
+  border-radius: 50%;
+  border: 1px solid #000;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.store .name {
+  width: max-content;
+}
+.store .hash {
+  display: flex;
+}
+
 .product {
   margin: 10px;
 }
@@ -94,6 +133,6 @@ export default {
 }
 .product .price .origin-price {
   font-size: 0.8em;
-  color: #b3b3b3;
+  color: #ddd;
 }
 </style>
