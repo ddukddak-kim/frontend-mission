8bbd4949e711e05d987fae0c4156f86144096d1d
kmkim <kkgg7527@gmail.com>
2022-01-16 15:53:09 +0900

    feat: add detail-component

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="item-info-page">
     <Title />
+    <Detail />
   </div>
 </template>
 
 <script>
 import Title from '@/components/Title.vue';
+import Detail from '@/components/Detail.vue';
 
 export default {
   name: 'ItemInfoPage',
   components: {
     Title,
+    Detail,
   },
   data() {
     return {};
