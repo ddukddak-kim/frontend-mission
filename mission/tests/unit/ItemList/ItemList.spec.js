91f6f18001ee4b582144ecaa27e6dcc71ef65695
kmkim <kkgg7527@gmail.com>
2022-01-23 04:00:57 +0900

    test: test code for navigation bar

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -13,4 +13,14 @@ describe('ItemListPage', () => {
 
     expect(wrapper.find('[data-test="store-name"]').exists()).toBe(true);
   });
+
+  it('redners navigation items', () => {
+    const wrapper = mount(ItemInfoPage);
+
+    // isExist Navigation
+    expect(wrapper.find('[data-test="navigation"]').exists()).toBe(true);
+
+    // has 4 Navigation-Items
+    expect(wrapper.find('[data-test="navigation-item"]').length).toBe(4);
+  });
 });
