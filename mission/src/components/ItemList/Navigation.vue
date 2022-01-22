0f4a6f3243cf4c702a688c341b4b5b6e690e449e
kmkim <kkgg7527@gmail.com>
2022-01-23 04:01:31 +0900

    add: navigation & item template

--- mission/src/components/ItemList/Navigation.vue
+++ mission/src/components/ItemList/Navigation.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="item-list-nav">
-    <font-awesome-icon :icon="['fas', 'phone']" />
+  <div class="navigation" data-test="navigation">
+    <Item iconType="home" class="select" />
+    <Item iconType="favored" />
+    <Item iconType="cart" />
+    <Item iconType="myPage" />
   </div>
 </template>
 
 <script>
+import Item from '@/components/ItemList/NavigationItem.vue';
+
 export default {
   name: 'Navigation',
+  components: { Item },
   props: {},
 };
 </script>
 
 <style>
-.item-list-nav {
+.navigation {
+  width: 100%;
+  min-height: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  border-top: 1px solid #ddd;
+}
+.navigation .select {
+  /*box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;*/
 }
 </style>
---
+++ mission/src/components/ItemList/NavigationItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-item" data-test="navigation-item">
+    <font-awesome-icon :icon="customIcon" />
+    <div>{{iconType}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationItem',
+  props: {
+    iconType: String,
+  },
+  computed: {
+    customIcon() {
+      const type = !this.iconType ? '' : this.iconType;
+      let icon = '';
+
+      switch (type) {
+        case 'home':
+          icon = ['fas', 'home'];
+          break;
+        case 'favored':
+          icon = ['fas', 'heart'];
+          break;
+        case 'cart':
+          icon = ['fas', 'shopping-cart'];
+          break;
+        case 'myPage':
+          icon = ['fas', 'phone'];
+          break;
+        default:
+          icon = ['fa', 'times'];
+          break;
+      }
+
+      return icon;
+    },
+  },
+};
+</script>
+
+<style>
+.nav-item {
+}
+</style>
