b222f0d53735c81cba598768952935630451bd9e
kmkim <kkgg7527@gmail.com>
2022-01-23 01:55:05 +0900

    chore: add fontawesome for icon

--- mission/src/main.js
+++ mission/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+library.add(fab, far, fas);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
