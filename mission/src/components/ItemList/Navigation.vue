8e9c86ae84b0168706c4dfb56c1f0e8623da451d
kmkim <kkgg7527@gmail.com>
2022-01-23 02:30:46 +0900

    feat: add navigationBar for itemList

---
+++ mission/src/components/ItemList/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="item-list-nav">
+    <font-awesome-icon :icon="['fas', 'phone']" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {},
+};
+</script>
+
+<style>
+.item-list-nav {
+}
+</style>
--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="item-list-page">
     <Header v-bind:storeName="store.name" data-test="store-name" />
+    <Navigation />
   </div>
 </template>
 
 <script>
 import Header from '@/components/ItemList/Header.vue';
+import Navigation from '@/components/ItemList/Navigation.vue';
 
 export default {
   name: 'ItemListPage',
-  components: { Header },
+  components: { Header, Navigation },
   data() {
     return {
       store: {
