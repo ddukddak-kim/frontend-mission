3cc35f0bf7a67230b9b62928107e03538d1831e8
kmkim <kkgg7527@gmail.com>
2022-01-23 16:23:51 +0900

    test: add test code for itemList

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,26 +1,52 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/views/ItemList.vue';
+import ItemListPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
   it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
+    const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
   });
 
   it('redners header items', () => {
-    const wrapper = mount(ItemInfoPage);
+    const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('[data-test="store-name"]').exists()).toBe(true);
   });
 
   it('redners navigation items', () => {
-    const wrapper = mount(ItemInfoPage);
+    const wrapper = mount(ItemListPage);
 
     // isExist Navigation
     expect(wrapper.find('[data-test="navigation"]').exists()).toBe(true);
 
     // has 4 Navigation-Items
-    expect(wrapper.find('[data-test="navigation-item"]').length).toBe(4);
+    // expect(wrapper.find('[data-test="navigation-item"]').length).toBe(4);
+  });
+
+  it('redners product-list items', () => {
+    const wrapper = mount(ItemListPage, {
+      data() {
+        return {
+          product: [
+            {
+              id: '1',
+              name: 'testData',
+              price: '10',
+              discountPrice: '',
+              description: 'test data',
+              /* eslint-disable global-require */
+              img: require('@/assets/img/title/store_01.jpg'),
+            },
+          ],
+        };
+      },
+    });
+
+    // if productData Length is even -> check empty-item added
+    expect(wrapper.find('[data-test="empty-item"]').exists()).toBe(true);
+
+    // if productData exist -> check item added
+    expect(wrapper.find('.item-list-item').exists()).toBe(true);
   });
 });
