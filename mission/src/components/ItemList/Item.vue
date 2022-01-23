35753bf0b5dd5412acd44f847f500b7ba65a9821
kmkim <kkgg7527@gmail.com>
2022-01-23 16:24:18 +0900

    feat: add product item template

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,12 +1,71 @@
 <template>
-  <div class="item-list-item"></div>
+  <div class="item-list-item">
+    <div v-if="isEmpty" class="empty"></div>
+    <div v-else class="item">
+      <div
+        class="image"
+        :style="{ 'background-image': `url('${ !product.img ? '' : product.img }')` }"
+      ></div>
+      <div class="price-info">
+        <a class="percent">11%</a>
+        <a>13,400 원</a>
+      </div>
+      <div class="product-name">
+        <a>product 1</a>
+      </div>
+      <div>
+        <a>desc</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ItemListItem',
+  props: {
+    product: Object,
+    isEmpty: Boolean,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
 <style>
+.item-list-item {
+  width: calc(50% - 10px);
+  margin-bottom: 10px;
+}
+
+.item-list-item .item > div {
+  width: max-content;
+  margin-bottom: 5px;
+}
+
+.item-list-item .item .image {
+  width: 100%;
+  height: 100px;
+  border: 2px solid#747474;
+  border-radius: 10px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.item .percent {
+  padding-right: 10px;
+  color: #d81a2c;
+}
+
+.item .price-info {
+  font-size: 1.2em;
+}
+
+.item .product-name {
+  font-size: 1.2em;
+  font-weight: bold;
+  color: #7a7878;
+}
 </style>
--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="item-list-page">
     <Header v-bind:storeName="store.name" data-test="store-name" />
+    <div class="item-list">
+      <Item v-for="(item, index) of product" :key="index" :product="item" />
+      <Item v-if="isEven" :isEmpty="true" data-test="empty-item" />
+    </div>
     <Navigation />
   </div>
 </template>
@@ -8,18 +12,79 @@
 <script>
 import Header from '@/components/ItemList/Header.vue';
 import Navigation from '@/components/ItemList/Navigation.vue';
+import Item from '@/components/ItemList/Item.vue';
 
 export default {
   name: 'ItemListPage',
-  components: { Header, Navigation },
+  components: { Header, Item, Navigation },
   data() {
     return {
       store: {
         name: 'My Shopping Mall',
       },
+      product: [
+        {
+          id: '1',
+          name: '잠옷',
+          price: '',
+          discountPrice: '',
+          description: '꿀잠가능합니다.',
+          /* eslint-disable global-require */
+          img: require('@/assets/img/title/store_01.jpg'),
+        },
+        {
+          id: '2',
+          name: '모자',
+          price: '',
+          discountPrice: '',
+          description: '따뜻한 모자예요.',
+          /* eslint-disable global-require */
+          img: require('@/assets/img/title/store_01.jpg'),
+        },
+        {
+          id: '3',
+          name: '장갑',
+          price: '',
+          discountPrice: '',
+          description: '터치장갑입니다.',
+          /* eslint-disable global-require */
+          img: require('@/assets/img/title/store_01.jpg'),
+        },
+        {
+          id: '4',
+          name: '넥타이',
+          price: '',
+          discountPrice: '',
+          description: '무지개색입니다.',
+          /* eslint-disable global-require */
+          img: require('@/assets/img/title/store_01.jpg'),
+        },
+        {
+          id: '5',
+          name: '폼롤러',
+          price: '',
+          discountPrice: '',
+          description: '스트레칭하기 좋아요.',
+          /* eslint-disable global-require */
+          img: require('@/assets/img/title/store_01.jpg'),
+        },
+      ],
     };
+  },
+  computed: {
+    isEven() {
+      return this.product.length % 2 === 1;
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.item-list {
+  width: 100%;
+  padding-bottom: 60px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
