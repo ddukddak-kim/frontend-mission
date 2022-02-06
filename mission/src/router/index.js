e90ef0217d7669144e69f3be68a06d8b99e77272
kmkim <kkgg7527@gmail.com>
2022-02-06 15:38:19 +0900

    feat: add itemInfoPage in router

--- mission/src/router/index.js
+++ mission/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ItemListPage from '@/views/ItemList.vue';
+import ItemInfoPage from '@/views/ItemInfo.vue';
 
 const routes = [
   {
@@ -10,10 +11,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/item',
+    name: 'ItemInfo',
+    component: ItemInfoPage,
   },
 ];
 
