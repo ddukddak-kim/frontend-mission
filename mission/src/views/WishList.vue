f073b92f702cd9178846d1bee5a26abf6e80618d
kmkim <kkgg7527@gmail.com>
2022-02-12 23:25:03 +0900

    refactor: refactor for getting WishList from axios API using repository pattern

---
+++ mission/src/views/WishList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="item-list-page">
+    <Header v-bind:storeName="store.name" data-test="store-name" />
+    <div class="item-list">
+      <Item
+        data-test="item"
+        v-for="(item, index) of wishes"
+        :key="index"
+        :product="item"
+        @click="moveDetail(item)"
+      />
+      <Item v-if="isEven" :isEmpty="true" data-test="empty-item" />
+    </div>
+    <Navigation />
+    <Loading />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/ItemList/Header.vue';
+import Navigation from '@/components/ItemList/Navigation.vue';
+import Item from '@/components/ItemList/Item.vue';
+import Loading from '@/components/Loading.vue';
+import RepositoryFactory from '@/repositories/RepositoryFactory';
+
+const WishRepository = RepositoryFactory.get('wish');
+
+export default {
+  name: 'WishList',
+  components: {
+    Header,
+    Item,
+    Navigation,
+    Loading,
+  },
+  data() {
+    return {
+      store: {
+        name: 'My Shopping Mall',
+      },
+      wishes: [],
+    };
+  },
+  methods: {
+    async getWishList() {
+      const { data } = await WishRepository.getItemList();
+      this.$store.state.isLoading = false;
+      this.wishes = data.items;
+    },
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    isEven() {
+      return this.products.length % 2 === 1;
+    },
+  },
+  created() {
+    this.getWishList();
+  },
+};
+</script>
+
+<style>
+.item-list {
+  width: 100%;
+  padding-bottom: 60px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
